e0c28a15449a4acd6fc9c6472ab36c5512b5e9ed
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2022-08-02 01:24:16 +0200

    todo-commit-1aug

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,11 @@
 /** @format */
 
 module.exports = function (app) {
-  app.get("/todo", function (req, res) {});
+  app.get("/todo", function (req, res) {
+    res.render("todo");
+  });
 
   app.post("/todo", function (req, res) {});
 
   app.delete("/todo", function (req, res) {});
 };
-
-  
