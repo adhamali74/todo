10e8e1424fd76d99259f8992b420834aa6e88ad3
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2022-08-27 19:22:53 +0200

    todo-commit-Add todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,23 @@
 /** @format */
 
+const bodyParser = require("body-parser");
+
 var data = [
   { item: "Ahly Match" },
   { item: "walk with Shalaby" },
   { item: "kick some coding ass" },
 ];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
   app.get("/todo", function (req, res) {
     res.render("todo", { todos: data });
   });
 
-  app.post("/todo", function (req, res) {});
+  app.post("/todo", urlencodedParser, function (req, res) {
+    data.push(req.body);
+    res.json(data);
+  });
 
   app.delete("/todo", function (req, res) {});
 };
